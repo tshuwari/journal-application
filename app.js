86df3351f34384710369bb46dec799c5c83aac92
igroz777 <igroz777@gmail.com>
2016-10-21 16:10:42 +0100

    Latest

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 var filename = "data.txt";
 
 var journalData = {};
 
-
+app.use(express.static(__dirname + '/public'));
 
 
 
@@ -20,38 +22,37 @@ app.get('/', function(req, res){
 });
 
 app.get('/journal/', function(req, res){
-	
-});
-
-app.get('/journal/data', function(req, res){
 	fs.readFile(filename, 'utf8', function(err, data){
 		journalData = JSON.parse(data);
-		
 		var links = [];
 		for(keys in journalData){
 			links.push(keys);
 		}
-		var htmlData = "";
-		for(var i = 0; i < links.length; i++){
-			htmlData += "<div class='journal-link'><div class='journal-link-text'><a href='/journal/";
-			htmlData += links[i];
-			htmlData += "'>" + links[i] + "</a></div></div>";
-		}
+		var temp = {journal: links};
+		res.render('index.ejs', temp);
+	});
+});
+
+
+app.get('/journal/new', function(req, res){
+	res.send(req.params.id);
+});
+
+app.post('/journal/new', function(req, res){
+	res.send(req.params.id);
+});
+
+app.get('/journal/:id', function(req, res){
+	fs.readFile(filename, 'utf8', function(err, data){
+		journalData = JSON.parse(data);
+
+
+				
+		
 		res.send(htmlData);
 	});
 });
 
-app.get('/journal/:id', function(req, res){
-	res.send(req.params.id);
-});
-
-app.get('/journal/:id/entry/', function(req, res){
-	res.send(req.params.id);
-});
-
-app.get('/journal/:id', function(req, res){
-	res.send(req.params.id);
-});
 
 app.listen(9999, function(){
 	console.log('Runnig server on 9999');
