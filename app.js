d109f321ff6c6a495ebe0e83a1bd3064d7b28a9d
igroz777 <igroz777@gmail.com>
2016-10-21 18:40:07 +0100

    Last commit. Added a bunch of stuff

--- app.js
+++ app.js
@@ -34,17 +34,16 @@ app.get('/journal/', function(req, res){
 });
 
 
+
 app.get('/journal/:id', function(req, res){
 	fs.readFile(filename, 'utf8', function(err, data){
 		journalData = JSON.parse(data);
 
 		var temp = {header: req.params.id};
-
 		var entries = journalData[req.params.id];
 		var entriesT = [];
 		for(keys in entries){
 			entriesT.push(entries[keys]);
-
 		}
 
 		res.render('journal-entries.ejs', {header: req.params.id,
@@ -52,12 +51,40 @@ app.get('/journal/:id', function(req, res){
 	});
 });
 
-app.get('/journal/new', function(req, res){
-	res.send(req.params.id);
+
+
+app.get('/journal/:id/new', function(req, res){
+	
+	res.render('journal-new-entry.ejs', {title: req.params.id});
 });
 
-app.post('/journal/new', function(req, res){
-	res.send(req.params.id);
+app.get('/journal/:id/create', function(req, res){
+	console.log(req.query.title);
+	console.log(req.query.body);
+	
+
+
+	fs.readFile(filename, 'utf8', function(err, data){
+		journalData = JSON.parse(data);
+
+		var tempL = 0;
+
+		for(keys in journalData[req.params.id]){
+			tempL += 1;
+		}
+
+		journalData[req.params.id][tempL] = {};
+		journalData[req.params.id][tempL].title = req.query.title;
+		journalData[req.params.id][tempL].body = req.query.body;
+
+		var writeData = JSON.stringify(journalData);
+
+		fs.writeFile(filename, writeData, function (err) {
+			console.log("Data written");
+			res.redirect('/journal/personal');
+		});
+	});
+
 });
 
 
